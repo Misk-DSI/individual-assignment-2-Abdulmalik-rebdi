b63b0e4c4ddff50dd0b7b1f6c1ea71e7182f1b5f
Abdulmalik-rebdi <Abdulmalik.rebdi@gmail.com>
2022-06-15 19:42:25 +0300

    found more info about the quality and volatile acidity and cerate bar plot for wine quality that shows a lot

--- src/wine_EDA.R
+++ src/wine_EDA.R
@@ -1,5 +1,6 @@
-library(gt)
 library(tidyverse)
+library(corrplot)
+library(gt)
 library(glue)
 
 
@@ -8,9 +9,11 @@ wine <- read.csv("./data/winequality-red.csv")
 
 ####
 
+##to look more to data
+summary(wine)
 
-# wineCor <-cor(mtcars)
-###
+##
+### it is symtric which make scense that the higher quality will be less and sciense quality ...
 ## this will show Correlation blue is a positive relationship  and red is a negative relationship 
 
 
@@ -24,17 +27,32 @@ corrplot(wineCor, type="upper", order="hclust",  )
 
 
 
-## what i am curious about is the range in each quality the box can easily show that 
+## what i am curious about is the range in each quality the box plot can easily show that 
 
 boxplot(wine$volatile.acidity~wine$quality,data=wine, main="Quality and volatile acidity RL",
-        xlab="quality", ylab="volatile acidity")
-
+        xlab="quality", ylab="volatile acidity" )
 ## we can clearly notice that the higher quality will have less volatile acidity 
+# if we observe the range only we can definitely can see that the range will show us the effect of volatile acidity with regards of quality    
+# this also show the it is right skewed to confirm 
+wine %>% 
+  group_by(quality) %>% 
+  summarise(AVG = mean(volatile.acidity)) %>% 
+  ggplot(aes(x =quality  , y= AVG)) + 
+  geom_bar(stat='identity')+ ##  identity will provide the y-values for the barplot, rather than counting the aggregate number 
+  labs(y="the avrage of  Volatile Acidity ", x=" Quality")
+ 
+ 
+     
 
 ##first insight 
 
 ##second insight 
 
+ggplot(wine, aes(x=quality )) +
+  geom_bar() + 
+  labs(y="Num of Observations", x="Wine Quality") 
+## from this barPlot will show us that the trends are medium quality we can clearly see that quality number 5 is 3 times higher than more 7&8
+## witch might be the market share of each quality 
 
 
 
@@ -52,5 +70,6 @@ wine %>%
   gt_preview()
  
 ########
+
 
   
